e0c64b54ced4436f90c501f67faa4ae4e2ae4358
Daniel Emaasit <daniel.emaasit@za.ibm.com>
2016-07-08 16:35:31 +0300

    first build

---
+++ R/trb_article.R
@@ -0,0 +1,27 @@
+#' TRB journal format.
+#'
+#' Format for creating submissions to TRB journals. Adapted from
+#' \href{https://www.elsevier.com/authors/author-schemas/latex-instructions}{https://www.elsevier.com/authors/author-schemas/latex-instructions}.
+#'
+#' @inheritParams rmarkdown::pdf_document
+#' @param ... Additional arguments to \code{rmarkdown::pdf_document}
+#'
+#' @return R Markdown output format to pass to
+#'   \code{\link[rmarkdown:render]{render}}
+#'
+#' @examples
+#'
+#' \dontrun{
+#' library(rmarkdown)
+#' draft("MyArticle.Rmd", template = "TRB_article", package = "rticles")
+#' }
+#'
+#' @export
+trb_article <- function(...,
+                             keep_tex = TRUE,
+                             md_extensions = c("-autolink_bare_uris")) {
+  inherit_pdf_document(...,
+                       template = find_resource("trb_article", "trb_template.tex"),
+                       keep_tex = keep_tex,
+                       md_extensions = md_extensions)
+}
